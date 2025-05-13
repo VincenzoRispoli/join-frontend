02f7e520ff905672e664a22cd76170fa50d4443e
VincenzoRispoli <150629139+VincenzoRispoli@users.noreply.github.com>
2025-05-14 00:45:54 +0200

    new commit

--- JS/contacts.js
+++ JS/contacts.js
@@ -144,7 +144,6 @@ async function createContact(event) {
     let randomColor = Math.floor(Math.random() * (badgeColors.length - 1));
     let badgeColor = badgeColors[randomColor];
     let newContact = new Contact(loggedUser.user_id, firstName.value, lastName.value, email.value, phone.value, badgeColor);
-    console.log(newContact);
     try {
         await postNewContact(newContact, loggedUser, firstName, lastName, email, phone);
     } catch (e) {
